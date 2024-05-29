c1193c9d11dfdf4737ca43337e709d4793638125
Bilal <bilalshaikh2937@gmail.com>
2024-05-29 18:01:11 +0530

    testing api on web server

--- server.js
+++ server.js
@@ -15,26 +15,39 @@ dotenv.config();
 const app = express(); // Create Express app
 const port = 4000; // Define port number
 
-// Middleware setup
-app.use(express.json()); // Parse JSON request bodies
-app.use(cors()); // Enable Cross-Origin Resource Sharing
+// // Middleware setup
+// app.use(express.json()); // Parse JSON request bodies
+// app.use(cors()); // Enable Cross-Origin Resource Sharing
 
-// Database connection
-connectDB(); // Call function to connect to the database
+// // Database connection
+// connectDB(); // Call function to connect to the database
 
-// Define API Endpoints
-app.use("/api/food", foodRouter); // Food-related API endpoints
-app.use("/images", express.static('uploads')); // Serve static images from 'uploads' directory
-app.use("/api/user", userRouter); // User-related API endpoints
-app.use("/api/cart", cartRouter); // Cart-related API endpoints
-app.use("/api/order", orderRouter); // Order-related API endpoints
+// // Define API Endpoints
+// app.use("/api/food", foodRouter); // Food-related API endpoints
+// app.use("/images", express.static('uploads')); // Serve static images from 'uploads' directory
+// app.use("/api/user", userRouter); // User-related API endpoints
+// app.use("/api/cart", cartRouter); // Cart-related API endpoints
+// app.use("/api/order", orderRouter); // Order-related API endpoints
 
-// Default route
-app.get("/", (req, res) => {
-    res.send("API working"); // Send a simple message indicating that the API is working
+// // Default route
+// app.get("/", (req, res) => {
+//     res.send("API working"); // Send a simple message indicating that the API is working
+// });
+
+
+app.get('/api/hello', (req, res) => {
+    res.status(200).json({ message: 'Hello, world!' });
+});
+
+app.get('*', (req, res) => {
+    res.status(404).json({ error: 'Not Found' });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
 
 // Start the server
-app.listen(port, () => {
-    console.log(`Server started on http://localhost:${port}`); // Log a message indicating that the server has started
-});
+// app.listen(port, () => {
+//     console.log(`Server started on http://localhost:${port}`); // Log a message indicating that the server has started
+// });
